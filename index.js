c8d10a5e7db9508f9b75d6b8d05a15cf5e1e85cd
jitendra-31 <jitendra.sahu@jda.com>
2022-10-18 10:52:53 +0530

    first commit -index.js and package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing express module(as a const)
+const express = require("express")
+
+//inintializing express app
+const app = express()
+
+app.listen(3000, () => {
+        console.log("Server running on port 3000")
+    }
+)
+
+app.use("/", (req, res) =>{ res.send("Welcome to node application")})
